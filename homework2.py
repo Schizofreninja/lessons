e44743651b361e04b8aa7d4a57660380557d96b5
Schizofreninja <ukann05@gmail.com>
2024-07-04 20:37:03 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -3,6 +3,6 @@ a = 12
 DZ = 'всего задач: ' + str(a)
 b = 1.5
 kvo_chasov = 'затрачено часов: ' + str(b)
-kurs = 'Курс: Python'
+kurs = 'Python'
 sr_time = 'среднее время выполнения: ' + str(b / a) + ' часа.'
-print(kurs, ',', DZ, ',', kvo_chasov, ',', sr_time)
+print('Курс: ', kurs, ',', DZ, ',', kvo_chasov, ',', sr_time)
