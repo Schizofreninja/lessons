4f1c49a000636c94fdf0bb1548eaf7342413b0d4
Schizofreninja <ukann05@gmail.com>
2024-07-04 17:56:22 +0300

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -2,9 +2,9 @@ example = 'Топинамбур' # 1
 print(example[:1]) # 2
 print(example[-1:]) # 3
 print(example[5:]) # 4
-print(example[::-1]) # 5
-print(example[1::2]) # 6
-
 # второй вариант решения задачи в п.4 (из интернета, но понравился)
 second_half = example[len(example)//2:]
+
 print(second_half)
+print(example[::-1]) # 5
+print(example[1::2]) # 6
