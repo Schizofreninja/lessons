d6d6388e772f2f0a9ab705f72a746fbb13e2a592
Schizofreninja <ukann05@gmail.com>
2024-07-04 17:50:14 +0300

    Строки и индексация строк

---
+++ homework1.py
@@ -0,0 +1,12 @@
+example = 'Топинамбур' # 1
+print(example[:1]) # 2
+print(example[-1:]) # 3
+print(example[5:]) # 4
+print(example[::-1]) # 5
+print(example[::2]) # 6
+
+# второй вариант решения задачи в п.4 (из интернета, но понравился)
+example = 'Топинамбур'
+first_half = example[:len(example)//2]
+second_half = example[len(example)//2:]
+print(second_half)
