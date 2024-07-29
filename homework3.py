3f0e3531329afcd02906cac0cf72010cb5b8fc14
Schizofreninja <ukann05@gmail.com>
2024-07-29 13:49:14 +0300

    Динамическая типизация
    Практическая работа по уроку "Динамическая типизация"

---
+++ homework3.py
@@ -0,0 +1,9 @@
+#Практическая работа по уроку "Динамическая типизация"
+name = 'Павел'
+print('Name: ', name)
+age = 21
+print('Age: ', age)
+age = age+10
+print('New age: ', age)
+is_student = True
+print('Is student: ',  is_student)
